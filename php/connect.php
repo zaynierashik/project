1d9cb7c38925e1c0b258ae116b977f7958cb60e2
zaynierashik <zaynierashik@gmail.com>
2023-09-15 12:48:07 +0545

    New Commit

--- php/connect.php
+++ php/connect.php
@@ -1,7 +1,7 @@
 <?php
     try{
         $conn = new PDO("mysql: host=localhost; dbname=database", "root", "");
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         echo "Connection failed: " . $e->getMessage();
     }
